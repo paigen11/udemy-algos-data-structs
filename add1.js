c49b4163bf68894a2890eeea3cfe35fb422e0085
Paige Niedringhaus <paigen11@gmail.com>
2019-05-03 18:10:55 -0400

    space complexity

--- add1.js
+++ add1.js
@@ -30,6 +30,7 @@ addUpTo2(1000000000);
 let t4 = now();
 console.log(`Time elapsed: ${(t4 - t3) / 1000} seconds`);
 
+// space complexity
 // big O allows us to talk formally about how the runtime
 // of an algorithm grows as the inputs grow
 
@@ -46,4 +47,21 @@ console.log(`Time elapsed: ${(t4 - t3) / 1000} seconds`);
 // rules of thumb
 // constants don't matter - o(2n) === o(n), o(500) === o(1), o(13n^2) === o(n^2)
 // smaller terms don't matter - o(n +10) === o(n), o(n^2 + 5n + 8) === o(n^2)
-//
+
+// logarithms - the inverse of exponentiation (log base 2(8) = 3 === 2^3 = 8)
+// or log base 2 (value) = exponent === 2^exponent = value
+
+/* the logarithm of a number roughly measures the number of times 
+you can divide a number by 2 before you get a value that's less than
+or equal to one */
+
+// logarithmic time comlexity is great
+// o log(n) is better than o(1)
+// o (n log n) is better than o(n)
+
+/* space complexity (auxiliary space complexity) in js
+  primitives are constant space (booleans, numbers, etc.)
+  strings require o(n) space (where n is string length)
+  reference types are generally o(n) where n is the length (for arrays) 
+  or the number of keys (for objects)
+*/
