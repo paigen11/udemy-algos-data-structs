27aff6413d8edfac66e830874a7a5236cc44c483
Paige Niedringhaus <paigen11@gmail.com>
2019-05-24 15:52:21 -0400

    setup for finding longest substring

---
+++ findLongestSubstringSlidingWindow.js
@@ -0,0 +1,18 @@
+/* write a function called findLongestSubstring, which accepts a string and returns the 
+length of the longest substring with all distinct characters
+
+example:
+- findLongestSubstring('') // 0
+- findLongestSubstring('rithmschool') // 7
+- findLongestSubstring('thisisawesome') // 6
+- findLongestSubstring('thecatinthehat') // 7
+- findLongestSubstring('bbbbb') // 1
+- findLongestSubstring('longestsubstring') // 8
+- findLongestSubstring('thisishowwedoit') // 6
+
+time complexity: o(n)
+*/
+
+function findLongestSubstring(str) {
+  // TBD;
+}
