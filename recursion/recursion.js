f767f14b3c92f3f4991b600b7984131fc48c0a6d
Paige Niedringhaus <paigen11@gmail.com>
2019-05-29 09:20:11 -0400

    reorganizing files and adding recursion

---
+++ recursion/recursion.js
@@ -0,0 +1,9 @@
+/* what is recursion? 
+
+- a process (a function in our case) that calls itself 
+
+it's everywhere: json.parse(), json.stringify(), etc. 
+document.getElementById and DOM traversal
+object traversal
+we see it with more complex data structures
+*/
