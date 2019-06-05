05f2c473d5187992f7e1cd24d838be3990f3e5d8
Paige Niedringhaus <paigen11@gmail.com>
2019-06-05 17:23:12 -0400

    recursion 101

--- recursion/recursion.js
+++ recursion/recursion.js
@@ -7,3 +7,32 @@ document.getElementById and DOM traversal
 object traversal
 we see it with more complex data structures
 */
+
+// the call stack
+// any time a function is invoked it's placed (pushed) to the top of the call stack
+// when JS sees the return keyword or when the function ends, the compiler removes it (pop)
+
+/* how recursive functions work: invoke the SAME function
+with a different input until you reach your base case
+
+base case: the condition where the recursion ends - 
+this is the most important concept to understand
+
+two essential parts of a recursive function:
+- base case
+- different input
+*/
+
+// first recursive function
+function countDown(num) {
+  if (num <= 0) {
+    console.log('All done!');
+    return;
+  }
+  console.log(num);
+  num--;
+  countDown(num);
+}
+
+console.log(countDown(5));
+console.log(countDown(3));
