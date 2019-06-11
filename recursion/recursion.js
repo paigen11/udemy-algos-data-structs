49d45f91089cab554a475fbca6b77ae83947f0d1
Paige Niedringhaus <paigen11@gmail.com>
2019-06-11 17:16:57 -0400

    finished up recursion notes

--- recursion/recursion.js
+++ recursion/recursion.js
@@ -68,3 +68,56 @@ function recurseFactorial(num) {
 
 console.log('Recursive factorial', recurseFactorial(3));
 console.log('Recursive factorial', recurseFactorial(4));
+
+// helper function recursion
+function collectOddValues(arr) {
+  let result = [];
+
+  function helper(helperInput) {
+    if (helperInput.length === 0) {
+      return;
+    }
+
+    if (helperInput[0] % 2 !== 0) {
+      result.push(helperInput[0]);
+    }
+    // here, the recursion happens inside the helper function and shrinks the array each time
+    helper(helperInput.slice(1));
+  }
+
+  helper(arr);
+
+  return result;
+}
+
+console.log(collectOddValues([1, 2, 3, 4, 5, 6, 7, 8, 9]));
+
+// purely recursive function
+function collectOddValuesRecurse(arr) {
+  let newArr = [];
+
+  if (arr.length === 0) {
+    return newArr;
+  }
+
+  if (arr[0] % 2 !== 0) {
+    newArr.push(arr[0]);
+  }
+
+  // at the end, all the arrays are concatenated together at the very end
+  newArr = newArr.concat(collectOddValuesRecurse(arr.slice(1)));
+  return newArr;
+}
+
+console.log(collectOddValuesRecurse([1, 2, 3, 4, 5]));
+
+/* recursion tips:
+- for arrays, use methods like slice, the spread operator and concat so you
+don't mutate the arrays
+
+- strings are immutable, so you need to use methods like slice,
+substr, or substring to make copies
+
+- to make copies of objects, use Object.assign, or the spread
+operator
+ */
