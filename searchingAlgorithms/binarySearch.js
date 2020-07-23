d601ad0df2d02dd00b4f9a5093441a83f96a5123
Paige Niedringhaus <paigen11@gmail.com>
2020-07-23 09:56:18 -0400

    another, more concise solution

--- searchingAlgorithms/binarySearch.js
+++ searchingAlgorithms/binarySearch.js
@@ -49,3 +49,17 @@ console.log(binarySearch([1, 2, 3, 4, 5], 2)); // 1
 console.log(binarySearch([1, 2, 3, 4, 5], 3)); // 2
 console.log(binarySearch([1, 2, 3, 4, 5], 5)); // 4
 console.log(binarySearch([1, 2, 3, 4, 5], 6)); // -1
+
+// another more condensed solution
+function binarySearch2(arr, val) {
+  let start = 0;
+  let end = arr.length - 1;
+  let middle = Math.floor((start + end) / 2);
+
+  while (arr[middle] !== val && start <= end) {
+    if (val < arr[middle]) end = middle - 1;
+    else start = middle + 1;
+    middle = Math.floor((start + end) / 2);
+  }
+  return arr[middle] === val ? middle : -1;
+}
